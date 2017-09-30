7da66035c7a9646f870b46c84d2670ef714c85e4
andrewtralongo <30714492+andrewtralongo@users.noreply.github.com>
2017-09-30 17:29:09 +1000

    isharePosts

--- ishare/assets/js/main.js
+++ ishare/assets/js/main.js
@@ -1,10 +1,100 @@
 $(document).ready(function() {
 
 
+    $('.footer.intro').click(function() {
+        $('.comments.intro').toggleClass('hide');
+    });
+    
     $('.footer.newWorldp1').click(function() {
-
         $('.comments.newWorldp1').toggleClass('hide');
-
+    });
+    
+    $('.footer.newWorldp2').click(function() {
+        $('.comments.newWorldp2').toggleClass('hide');
+    });
+    
+    $('.footer.newWorldp3').click(function() {
+        $('.comments.newWorldp3').toggleClass('hide');
+    });
+    
+    $('.footer.twoLivesQ').click(function() {
+        $('.comments.twoLivesQ').toggleClass('hide');
+    });
+    
+    $('.footer.aloneTogetherp1').click(function() {
+        $('.comments.aloneTogetherp1').toggleClass('hide');
+    });
+    
+    $('.footer.aloneTogetherp2').click(function() {
+        $('.comments.aloneTogetherp2').toggleClass('hide');
+    });
+    
+    $('.footer.conversationQ').click(function() {
+        $('.comments.conversationQ').toggleClass('hide');
+    });
+    
+    $('.footer.disconnectedp1').click(function() {
+        $('.comments.disconnectedp1').toggleClass('hide');
+    });
+    
+    $('.footer.disconnectedp2').click(function() {
+        $('.comments.disconnectedp2').toggleClass('hide');
+    });
+    
+    $('.footer.unpluggingJoyQ').click(function() {
+        $('.comments.unpluggingJoyQ').toggleClass('hide');
+    });
+    
+    $('.footer.140charp1').click(function() {
+        $('.comments.140charp1').toggleClass('hide');
+    });
+    
+    $('.footer.smartestRoomQ').click(function() {
+        $('.comments.smartestRoomQ').toggleClass('hide');
+    });
+    
+    $('.footer.140charp2').click(function() {
+        $('.comments.140charp2').toggleClass('hide');
+    });
+    
+    $('.footer.trumpQ').click(function() {
+        $('.comments.trumpQ').toggleClass('hide');
+    });
+    
+    $('.footer.projectedIDp1').click(function() {
+        $('.comments.projectedIDp1').toggleClass('hide');
+    });
+    
+    $('.footer.renderedIDQ').click(function() {
+        $('.comments.renderedIDQ').toggleClass('hide');
+    });
+    
+    $('.footer.projectedIDp2').click(function() {
+        $('.comments.projectedIDp2').toggleClass('hide');
+    });
+    
+    $('.footer.girlsVid').click(function() {
+        $('.comments.girlsVid').toggleClass('hide');
+    });
+    
+    $('.footer.projectedIDp3').click(function() {
+        $('.comments.projectedIDp3').toggleClass('hide');
+    });
+    
+    $('.footer.projectedIDp4').click(function() {
+        $('.comments.projectedIDp4').toggleClass('hide');
+    });
+    
+    $('.footer.projectedIDp5').click(function() {
+        $('.comments.projectedIDp5').toggleClass('hide');
+    });
+    
+    $('.footer.projectedIDp6').click(function() {
+        $('.comments.projectedIDp6').toggleClass('hide');
+    });
+    
+    $('.footer.conc').click(function() {
+        $('.comments.conc').toggleClass('hide');
     });
 
 });
