797259feccc2ea9f4e41d1324cfd253c1cc4285c
andrewtralongo <30714492+andrewtralongo@users.noreply.github.com>
2017-09-30 13:04:06 +1000

    ishareFooter

--- ishare/assets/js/main.js
+++ ishare/assets/js/main.js
@@ -1,27 +1,10 @@
 $(document).ready(function() {
 
 
-// Add jQuery here
+    $('.footer.newWorldp1').click(function() {
 
+        $('.comments.newWorldp1').toggleClass('hide');
 
+    });
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-  });
+});
