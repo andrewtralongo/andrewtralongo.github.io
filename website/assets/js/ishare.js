f74b866dd589f0728579bc4c7b9e8556812c6ab0
andrewtralongo <30714492+andrewtralongo@users.noreply.github.com>
2017-10-11 16:41:00 +1100

    postFooter

--- website/assets/js/ishare.js
+++ website/assets/js/ishare.js
@@ -23,107 +23,130 @@ $(document).ready(function() {
     
     
 
-    $('.footer.intro').click(function() {
+    $('.fLike').click(function() {
+        $(this).toggleClass('active');
+    });
+    $('.fComment').click(function() {
+        $(this).toggleClass('active');
+    });
+    
+    $('.fComment.intro').click(function() {
         $('.comments.intro').toggleClass('hide');
     });
     
-    $('.footer.newWorldp1').click(function() {
+    $('.fComment.newWorldp1').click(function() {
         $('.comments.newWorldp1').toggleClass('hide');
     });
     
-    $('.footer.newWorldp2').click(function() {
+    $('.fComment.newWorldp2').click(function() {
         $('.comments.newWorldp2').toggleClass('hide');
     });
     
-    $('.footer.newWorldp3').click(function() {
+    $('.fComment.rateMe').click(function() {
+        $('.comments.rateMe').toggleClass('hide');
+    });
+    
+    $('.fComment.newWorldp3').click(function() {
         $('.comments.newWorldp3').toggleClass('hide');
     });
     
-    $('.footer.twoLivesQ').click(function() {
+    $('.fComment.socialRoulette').click(function() {
+        $('.comments.socialRoulette').toggleClass('hide');
+    });
+    
+    $('.fComment.twoLivesQ').click(function() {
         $('.comments.twoLivesQ').toggleClass('hide');
     });
     
-    $('.footer.aloneTogetherp1').click(function() {
+    $('.fComment.aloneTogetherp1').click(function() {
         $('.comments.aloneTogetherp1').toggleClass('hide');
     });
     
-    $('.footer.aloneTogetherp2').click(function() {
+    $('.fComment.siri').click(function() {
+        $('.comments.siri').toggleClass('hide');
+    });
+    
+    $('.fComment.aloneTogetherp2').click(function() {
         $('.comments.aloneTogetherp2').toggleClass('hide');
     });
     
-    $('.footer.conversationQ').click(function() {
+    $('.fComment.conversationQ').click(function() {
         $('.comments.conversationQ').toggleClass('hide');
     });
     
-    $('.footer.disconnectedp1').click(function() {
+    $('.fComment.disconnectedp1').click(function() {
         $('.comments.disconnectedp1').toggleClass('hide');
     });
     
-    $('.footer.disconnectedp2').click(function() {
+    $('.fComment.disconnectedp2').click(function() {
         $('.comments.disconnectedp2').toggleClass('hide');
     });
     
-    $('.footer.unpluggingJoyQ').click(function() {
+    $('.fComment.unpluggingJoyQ').click(function() {
         $('.comments.unpluggingJoyQ').toggleClass('hide');
     });
     
-    $('.footer.140charp1').click(function() {
+    $('.fComment.facebook').click(function() {
+        $('.comments.facebook').toggleClass('hide');
+    });
+    
+    $('.fComment.140charp1').click(function() {
         $('.comments.140charp1').toggleClass('hide');
     });
     
-    $('.footer.smartestRoomQ').click(function() {
+    $('.fComment.smartestRoomQ').click(function() {
         $('.comments.smartestRoomQ').toggleClass('hide');
     });
     
-    $('.footer.140charp2').click(function() {
+    $('.fComment.140charp2').click(function() {
         $('.comments.140charp2').toggleClass('hide');
     });
     
-    $('.footer.trumpQ').click(function() {
+    $('.fComment.trumpQ').click(function() {
         $('.comments.trumpQ').toggleClass('hide');
     });
     
-    $('.footer.projectedIDp1').click(function() {
+    $('.fComment.projectedIDp1').click(function() {
         $('.comments.projectedIDp1').toggleClass('hide');
     });
     
-    $('.footer.renderedIDQ').click(function() {
+    $('.fComment.renderedIDQ').click(function() {
         $('.comments.renderedIDQ').toggleClass('hide');
     });
     
-    $('.footer.projectedIDp2').click(function() {
+    $('.fComment.projectedIDp2').click(function() {
         $('.comments.projectedIDp2').toggleClass('hide');
     });
     
-    $('.footer.girlsVid').click(function() {
+    $('.fComment.girlsVid').click(function() {
         $('.comments.girlsVid').toggleClass('hide');
     });
     
-    $('.footer.projectedIDp3').click(function() {
+    $('.fComment.projectedIDp3').click(function() {
         $('.comments.projectedIDp3').toggleClass('hide');
     });
     
-    $('.footer.projectedIDp4').click(function() {
+    $('.fComment.projectedIDp4').click(function() {
         $('.comments.projectedIDp4').toggleClass('hide');
     });
     
-    $('.footer.projectedIDp5').click(function() {
+    $('.fComment.projectedIDp5').click(function() {
         $('.comments.projectedIDp5').toggleClass('hide');
     });
     
-    $('.footer.the1975insta1').click(function() {
+    $('.fComment.the1975insta1').click(function() {
         $('.comments.the1975insta1').toggleClass('hide');
     });
     
-    $('.footer.projectedIDp6').click(function() {
+    $('.fComment.projectedIDp6').click(function() {
         $('.comments.projectedIDp6').toggleClass('hide');
     });
     
-    $('.footer.the1975insta2').click(function() {
+    $('.fComment.the1975insta2').click(function() {
         $('.comments.the1975insta2').toggleClass('hide');
     });
     
-    $('.footer.conc').click(function() {
+    $('.fComment.conc').click(function() {
         $('.comments.conc').toggleClass('hide');
     });
 
