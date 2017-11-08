79d405e9c03f3a9032f617ea74a503d99b6849b2
andrewtralongo <30714492+andrewtralongo@users.noreply.github.com>
2017-11-08 13:30:04 +1100

    60-120

--- website/assets/js/ishare-scroll.js
+++ website/assets/js/ishare-scroll.js
@@ -1,16 +1,16 @@
 $(document).ready(function() {
 
-   $("html, body").animate({ scrollTop: $(document).height() }, 60000);
+   $("html, body").animate({ scrollTop: $(document).height() }, 120000);
     setTimeout(function() {
-       $('html, body').animate({scrollTop:0}, 60000); 
-    },60000);
+       $('html, body').animate({scrollTop:0}, 120000); 
+    },120000);
   var scrolltopbottom =  setInterval(function(){
          // 10000 - it will take 10 secound in total from the top of the page to the bottom
-    $("html, body").animate({ scrollTop: $(document).height() }, 60000);
+    $("html, body").animate({ scrollTop: $(document).height() }, 120000);
     setTimeout(function() {
-       $('html, body').animate({scrollTop:0}, 60000); 
-    },60000);
+       $('html, body').animate({scrollTop:0}, 120000); 
+    },120000);
 
-    },120000); 
+    },240000); 
    
 });
