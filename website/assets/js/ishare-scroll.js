a2150fe2b1fb6124728be99e4044459f047a7dc5
andrewtralongo <30714492+andrewtralongo@users.noreply.github.com>
2018-07-26 23:37:18 +1000

    REdesign180726
    Inital Uplaod

--- website/assets/js/ishare-scroll.js
+++ website/assets/js/ishare-scroll.js
@@ -2,15 +2,15 @@ $(document).ready(function() {
 
    $("html, body").animate({ scrollTop: $(document).height() }, 120000);
     setTimeout(function() {
-       $('html, body').animate({scrollTop:0}, 120000); 
-    },120000);
+       $('html, body').animate({scrollTop:0}, 240000); 
+    },240000, "linear");
   var scrolltopbottom =  setInterval(function(){
          // 10000 - it will take 10 secound in total from the top of the page to the bottom
     $("html, body").animate({ scrollTop: $(document).height() }, 120000);
     setTimeout(function() {
-       $('html, body').animate({scrollTop:0}, 120000); 
-    },120000);
+       $('html, body').animate({scrollTop:0}, 240000); 
+    },240000, "linear");
 
-    },240000); 
+    },480000); 
    
 });
