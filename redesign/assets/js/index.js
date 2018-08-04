1401cd4cc1d23ab22c32e2a14198a2c0a38eb2ac
andrewtralongo <30714492+andrewtralongo@users.noreply.github.com>
2018-08-04 17:38:26 +1000

    update4-08

--- redesign/assets/js/index.js
+++ redesign/assets/js/index.js
@@ -1,23 +1,103 @@
 $(document).ready(function() {
+    
+    function fnLogoBlack() {
+        $('.logoWhite').addClass('hide');
+        $('.logoLine').addClass('colBlack');
+        $('.herotxt').addClass('txtColBlack');
+    }
+            
+    function fnLogoWhite() {
+        $('.logoBlack').addClass('hide');
+        $('.logoLine').addClass('colWhite');
+        $('.herotxt').addClass('txtColWhite');
+    }
+
 
     setTimeout(function () {
         
-        var x = Math.floor(Math.random() * 2) + 1;
-        
-        if (x == 1){
-            $('.headerBg').addClass("hero1");
-            $('.logoBlack').addClass('hide');
-            $('.logoLine').addClass('colWhite');
-            $('.herotxt').addClass('txtColWhite');
+//        -- Mobile Backgrounds --
+        if ($(window).width() <= 320) {
+            
+           if (x == 1){
+            $('.headerBg').addClass("mobileHero1");
+            fnLogoWhite();
         }
         
         if (x == 2){
-            $('.headerBg').addClass("hero2");
-            $('.logoWhite').addClass('hide');
-            $('.logoLine').addClass('colBlack');
-            $('.herotxt').addClass('txtColBlack');
+            $('.headerBg').addClass("mobileHero2");
+            fnLogoWhite();
+        }
+        
+        if (x == 3){
+            $('.headerBg').addClass("mobileHero3");
+            fnLogoWhite();
+        } 
+            
+        }
+        
+        var x = Math.floor(Math.random() * 11) + 1;
+        
+//      Desktop Backgrounds
+        
+        if ($(window).width() > 320) {
+        
+        if (x == 1){
+            $('.headerBg').addClass("desktopHero1");
+            fnLogoWhite();
+        }
+        
+        if (x == 2){
+            $('.headerBg').addClass("desktopHero2");
+            fnLogoWhite();
+        }
+        
+        if (x == 3){
+            $('.headerBg').addClass("desktopHero3");
+            fnLogoWhite();
+        }
+        
+        if (x == 4){
+            $('.headerBg').addClass("desktopHero4");
+            fnLogoWhite();
+        }
+        
+        if (x == 5){
+            $('.headerBg').addClass("desktopHero5");
+            fnLogoWhite();
+        }
+        
+        if (x == 6){
+            $('.headerBg').addClass("desktopHero6");
+            fnLogoBlack();
+        }
+        
+        if (x == 7){
+            $('.headerBg').addClass("desktopHero7");
+            fnLogoWhite();
+        }
+        
+        if (x == 8){
+            $('.headerBg').addClass("desktopHero8");
+            fnLogoBlack();
+        }
+        
+        if (x == 9){
+            $('.headerBg').addClass("desktopHero9");
+            fnLogoWhite();
+        }
+        
+        if (x == 10){
+            $('.headerBg').addClass("desktopHero10");
+            fnLogoWhite();
+        }
+        
+        if (x == 11){
+            $('.headerBg').addClass("desktopHero11");
+            fnLogoWhite();
+        }
+            
         }
         
     }, 0);
     
-  });
+});
--- redesign/assets/js/main.js
+++ redesign/assets/js/main.js
@@ -21,16 +21,87 @@ $(document).ready(function() {
 
     setTimeout(function () {
         
-        var x = Math.floor(Math.random() * 2) + 1;
-        
-        if (x == 1){
-            $('.headerBg').addClass("hero1");
+//        -- Mobile Backgrounds --
+        if ($(window).width() <= 320) {
+            
+           if (x == 1){
+            $('.headerBg').addClass("mobileHero1");
             $('#my_div').fnLogoBlack();
         }
         
         if (x == 2){
-            $('.headerBg').addClass("hero2");
+            $('.headerBg').addClass("mobileHero2");
             $('#my_div').fnLogoWhite;
+        }
+        
+        if (x == 3){
+            $('.headerBg').addClass("mobileHero3");
+            $('#my_div').fnLogoWhite;
+        } 
+            
+        }
+        
+        var x = Math.floor(Math.random() * 11) + 1;
+        
+//      Desktop Backgrounds
+        
+        if ($(window).width() > 320) {
+        
+        if (x == 1){
+            $('.headerBg').addClass("desktopHero1");
+            $('#my_div').fnLogoBlack();
+        }
+        
+        if (x == 2){
+            $('.headerBg').addClass("desktopHero2");
+            $('#my_div').fnLogoWhite;
+        }
+        
+        if (x == 3){
+            $('.headerBg').addClass("desktopHero3");
+            $('#my_div').fnLogoWhite;
+        }
+        
+        if (x == 4){
+            $('.headerBg').addClass("desktopHero4");
+            $('#my_div').fnLogoWhite;
+        }
+        
+        if (x == 5){
+            $('.headerBg').addClass("desktopHero5");
+            $('#my_div').fnLogoWhite;
+        }
+        
+        if (x == 6){
+            $('.headerBg').addClass("desktopHero6");
+            $('#my_div').fnLogoWhite;
+        }
+        
+        if (x == 7){
+            $('.headerBg').addClass("desktopHero7");
+            $('#my_div').fnLogoWhite;
+        }
+        
+        if (x == 8){
+            $('.headerBg').addClass("desktopHero8");
+            $('#my_div').fnLogoWhite;
+        }
+        
+        if (x == 9){
+            $('.headerBg').addClass("desktopHero9");
+            $('#my_div').fnLogoWhite;
+        }
+        
+        if (x == 10){
+            $('.headerBg').addClass("desktopHero10");
+            $('#my_div').fnLogoWhite;
+        }
+        
+        if (x == 11){
+            $('.headerBg').addClass("desktopHero11");
+            $('#my_div').fnLogoWhite;
+        }
+            
         }
         
     }, 0);
