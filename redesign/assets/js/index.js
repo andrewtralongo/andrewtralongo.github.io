00b0ac5a5887428b88a7327e57d2cb6729c09c47
andrewtralongo <30714492+andrewtralongo@users.noreply.github.com>
2018-08-05 21:01:51 +1000

    designUpdate

--- redesign/assets/js/index.js
+++ redesign/assets/js/index.js
@@ -3,12 +3,18 @@ $(document).ready(function() {
     function fnLogoBlack() {
         $('.logoWhite').addClass('hide');
         $('.logoLine').addClass('colBlack');
+    }
+    
+    function fnTitleBlack() {
         $('.herotxt').addClass('txtColBlack');
     }
             
     function fnLogoWhite() {
         $('.logoBlack').addClass('hide');
         $('.logoLine').addClass('colWhite');
+    }
+    
+    function fnTitleWhite() {
         $('.herotxt').addClass('txtColWhite');
     }
 
@@ -21,16 +27,19 @@ $(document).ready(function() {
            if (x == 1){
             $('.headerBg').addClass("mobileHero1");
             fnLogoWhite();
+            fnTitleWhite();
         }
         
         if (x == 2){
             $('.headerBg').addClass("mobileHero2");
             fnLogoWhite();
+            fnTitleWhite();
         }
         
         if (x == 3){
             $('.headerBg').addClass("mobileHero3");
             fnLogoWhite();
+            fnTitleWhite();
         } 
             
         }
@@ -44,56 +53,67 @@ $(document).ready(function() {
         if (x == 1){
             $('.headerBg').addClass("desktopHero1");
             fnLogoWhite();
+            fnTitleWhite();
         }
         
         if (x == 2){
             $('.headerBg').addClass("desktopHero2");
             fnLogoWhite();
+            fnTitleWhite();
         }
         
         if (x == 3){
             $('.headerBg').addClass("desktopHero3");
             fnLogoWhite();
+            fnTitleWhite();
         }
         
         if (x == 4){
             $('.headerBg').addClass("desktopHero4");
             fnLogoWhite();
+            fnTitleWhite();
         }
         
         if (x == 5){
             $('.headerBg').addClass("desktopHero5");
-            fnLogoWhite();
+            fnLogoBlack();
+            fnTitleWhite();
         }
         
         if (x == 6){
             $('.headerBg').addClass("desktopHero6");
             fnLogoBlack();
+            fnTitleWhite();
         }
         
         if (x == 7){
             $('.headerBg').addClass("desktopHero7");
             fnLogoWhite();
+            fnTitleWhite();
         }
         
         if (x == 8){
             $('.headerBg').addClass("desktopHero8");
-            fnLogoBlack();
+            fnLogoWhite();
+            fnTitleWhite();
         }
         
         if (x == 9){
             $('.headerBg').addClass("desktopHero9");
             fnLogoWhite();
+            fnTitleWhite();
         }
         
         if (x == 10){
             $('.headerBg').addClass("desktopHero10");
             fnLogoWhite();
+            fnTitleWhite();
         }
         
         if (x == 11){
             $('.headerBg').addClass("desktopHero11");
             fnLogoWhite();
+            fnTitleWhite();
         }
             
         }
